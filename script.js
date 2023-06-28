b938d36b18f5a1e56d0323dc65a9042cb73fddb4
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-28 13:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonText = button.textContent;
+    const currentValue = input.value;
+    
+    if (buttonText === 'C') {
+      input.value = '';
+    } else if (buttonText === '=') {
+      try {
+        const result = eval(currentValue);
+        input.value = result;
+      } catch (error) {
+        input.value = 'Error';
+      }
+    } else {
+      input.value += buttonText;
+    }
+  });
+});
